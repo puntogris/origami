2f4957b39fc83d80082ad4951c936dc6c07ce0c2
puntogris <rosariodota3@gmail.com>
2024-09-10 13:25:43 -0300

    fix: height in mobile devices

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 	<title>origami.puntogris</title>
 </svelte:head>
 
-<div class="container mx-auto flex min-h-screen max-w-5xl flex-col p-6">
+<div class="container mx-auto flex min-h-dvh max-w-5xl flex-col p-6">
 	<header class="flex items-center gap-2">
 		<OrigamiIcon class="h-7 w-7" />
 		<h1 class="text-2xl font-medium">origami.</h1>
